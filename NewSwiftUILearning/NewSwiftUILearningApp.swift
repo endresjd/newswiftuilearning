0f1fa5f60704d9cc8e5317e12372175a2d0e8c86
John Endres <endres@macplugins.com>
2023-11-11 12:07:57 -0500

    Initial Commit

---
+++ NewSwiftUILearning/NewSwiftUILearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewSwiftUILearningApp.swift
+//  NewSwiftUILearning
+//
+//  Created by John Endres on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NewSwiftUILearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
