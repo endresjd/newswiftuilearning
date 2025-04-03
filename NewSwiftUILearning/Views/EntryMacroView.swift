8873929aa690c3493bfa4b5ee6b47ce089ee9425
John Endres <endres@macplugins.com>
2025-04-03 17:18:34 -0400

    Changed to optional Binding

--- NewSwiftUILearning/Views/EntryMacroView.swift
+++ NewSwiftUILearning/Views/EntryMacroView.swift
@@ -9,26 +9,56 @@ import SwiftUI
 
 // Creates an environment values, transaction, container values, or focused values entry
 extension EnvironmentValues {
+    /// Integer passed down to subviews
     @Entry var entryExampleValue = 10
+    
+    /// A binding to a Bool that indicates if a sheet with a string should be shown
+    @Entry var showString: Binding<Bool>?
 }
-
+ 
 struct SubView: View {
     @Environment(\.entryExampleValue) private var value: Int
-    
+    @Environment(\.showString) private var showString: Binding<Bool>?
+ 
     var body: some View {
-        Text("The value is \(value)")
+        VStack {
+            Text("The value is \(value)")
+            
+            if let showString {
+                Button("Show string") {
+                    showString.wrappedValue = true
+                }
+            }
+        }
     }
 }
-
+ 
 struct EntryMacroView: View {
+    @State private var isPresented = false
+ 
     var body: some View {
         SubView()
             .environment(\.entryExampleValue, 42)
-
+        
+        Divider()
+ 
         SubView()
+        
+        Divider()
+ 
+        SubView()
+            .environment(\.entryExampleValue, 100)
+            .environment(\.showString, $isPresented)
+            .fullScreenCover(isPresented: $isPresented) {
+                Text("John")
+                
+                Button("OK") {
+                    isPresented = false
+                }
+            }
     }
 }
-
+ 
 #Preview {
     EntryMacroView()
 }
