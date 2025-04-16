d4f4d903dbc76f4720a99dcbd8a229c5807b13cf
John Endres <endres@macplugins.com>
2025-04-16 16:25:22 -0400

    More customizations

--- NewSwiftUILearning/Views/SafeAreaExample.swift
+++ NewSwiftUILearning/Views/SafeAreaExample.swift
@@ -22,11 +22,24 @@ import SwiftUI
 ///     * [Adding a background to your view](https://www.createwithswift.com/placing-ui-components-within-the-safe-area-inset)
 ///     * [The safe area](https://www.swiftuifieldguide.com/layout/safe-area)
 struct SafeAreaExample: View {
+    /// Toggle going into the safe area on and off
     @State private var ignoreSafeArea = true
-    @State private var addPadding = false
+    
+    /// Show a view in the top safe area
     @State private var showTop = true
+    
+    /// Show a view in the bottom safe area
     @State private var showBottom = true
     
+    /// The amount of spacing to add to the top and bottom safe areas
+    @State private var padding = 0.0
+    
+    /// Amount of spacing to give the top safe area inset view
+    @State private var topSpacing = 0.0
+    
+    /// Amount of spacing to give the bottom safe area inset view
+    @State private var bottomSpacing = 0.0
+
     var body: some View {
         LinearGradient(colors: [.blue, .purple], startPoint: .top, endPoint: .bottom)
             .padding(.horizontal)
@@ -36,11 +49,34 @@ struct SafeAreaExample: View {
             }
             .overlay {
                 VStack {
-                    Toggle("Ignore Safe Area", isOn: $ignoreSafeArea)
-                    Toggle("Safe area padding", isOn: $addPadding)
+                    Toggle("Shade Safe Area", isOn: $ignoreSafeArea)
                     Toggle("Add to top area", isOn: $showTop)
                     Toggle("Add to bottom area", isOn: $showBottom)
                     
+                    LabeledContent {
+                        Stepper(value: $padding, in: 0...100, step: 5) {
+                            Text(padding.formatted(.number))
+                        }
+                    } label: {
+                        Text("Safe area padding:")
+                    }
+                    
+                    LabeledContent {
+                        Stepper(value: $topSpacing, in: 0...100, step: 5) {
+                            Text(topSpacing.formatted(.number))
+                        }
+                    } label: {
+                        Text("Top area spacing:")
+                    }
+                    
+                    LabeledContent {
+                        Stepper(value: $bottomSpacing, in: 0...100, step: 5) {
+                            Text(bottomSpacing.formatted(.number))
+                        }
+                    } label: {
+                        Text("Bottom area spacing:")
+                    }
+
                     Spacer()
                     
                     Text("Bottom of view")
@@ -48,22 +84,18 @@ struct SafeAreaExample: View {
                 .foregroundColor(.white)
                 .padding(.horizontal)
             }
-            .if(addPadding) { view in
-                // Adds the provided insets into the safe area of this view.
-                view
-                    .safeAreaPadding([.top, .bottom], 64.0)
-            }
+            .safeAreaPadding([.top, .bottom], padding)
             .if(showTop) { view in
                 // allows us to insert additional content along a specified edge of a view’s safe area.
                 view
-                    .safeAreaInset(edge: .top, spacing: 0) {
+                    .safeAreaInset(edge: .top, spacing: topSpacing) {
                         safeAreaComponent(text: "Top Safe Area", symbolName: "globe")
                     }
             }
             .if(showBottom) { view in
                 // allows us to insert additional content along a specified edge of a view’s safe area.
                 view
-                    .safeAreaInset(edge: .bottom, spacing: 0) {
+                    .safeAreaInset(edge: .bottom, spacing: bottomSpacing) {
                         safeAreaComponent(text: "Bottom Safe Area", symbolName: "star.fill")
                     }
             }
